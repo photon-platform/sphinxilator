63ccb523da493ba0be71105694caa2d929a2361c
phiarchitect <phi@phiarchitect.com>
2024-05-04 20:05:41 -0700

    test theme

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -2,6 +2,8 @@ from ablog.conf import *
 import photon_platform.sphinxilator as module
 version = module.__version__
 
+import photon_platform.sphinxilator.theme
+
 
 org = "photon-platform"
 org_name = "photon-platform"
@@ -10,3 +12,5 @@ repo = "sphinxilator"
 repo_name = "sphinxilator"
 
 setup_globals(org, org_name, repo, repo_name)
+
+html_theme = "sphinxilator_theme"
